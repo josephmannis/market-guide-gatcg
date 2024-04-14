3a99dbb509e4c7d538d029277ecd7db85adeb123
Joseph Annis <annis.jo@husky.neu.edu>
2024-04-13 18:53:38 -0700

    Data Update: Sat Apr 13 18:53:38 PDT 2024

--- src/data-scraping/cron-job.sh
+++ src/data-scraping/cron-job.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+cd /Users/prince/Documents/dev/presentable-projects/market-guide/src/data-scraping/ || exit
+
 tsx ./src/data-scraping/get-full-card-index.ts
+
+cd ../../
 
 git add .
 git commit -m "Data Update: $(date)"
