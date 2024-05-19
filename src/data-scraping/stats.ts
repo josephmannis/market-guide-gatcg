9386a6d889a5027495981280aa573388d8b916e6
Joseph Annis <annis.jo@husky.neu.edu>
2024-05-18 18:38:23 -0700

    Data Update: Sat May 18 18:38:23 PDT 2024

--- src/data-scraping/stats.ts
+++ src/data-scraping/stats.ts
@@ -1,6 +1,19 @@
 import cardGroupMap from '../data/group-card-map.json';
 import productGroups from '../data/product-groups.json';
 import _ from 'lodash';
+import { writeFileSync } from 'node:fs';
+function makeCsv(headers: string[], data: string[][]): string {
+  let csvContent = headers.join(',');
+  csvContent += '\r\n';
+  // "data:text/csv;charset=utf-8,";
+
+  data.forEach(function (rowArray) {
+    const row = rowArray.join(',');
+    csvContent += row + '\r\n';
+  });
+
+  return csvContent;
+}
 
 function getCollectionPrice() {
   const groups = ['23338', '23212', '23128'];
@@ -44,4 +57,28 @@ function getCollectionPrice() {
   console.log(fullPrice);
 }
 
-getCollectionPrice();
+function getFoilsInOrder() {
+  const groups = ['23338', '23212', '23128'];
+  let foilCards: { name: string; price: number }[] = [];
+
+  for (const groupId of groups) {
+    const cards = cardGroupMap[groupId];
+    foilCards = foilCards.concat(
+      cards
+        .filter((c) => !!c.foilPrices.midPrice)
+        .map((c) => ({
+          name: c.name,
+          price: c.foilPrices.midPrice,
+        }))
+    );
+  }
+
+  const prunedPrices: string[][] = foilCards
+    .filter((c) => c.price <= 3)
+    .sort((a, b) => a.price - b.price)
+    .map((c) => [c.name, c.price.toString(10)]);
+
+  writeFileSync('foilprices.csv', makeCsv(['name', 'price'], prunedPrices), 'utf-8');
+}
+
+getFoilsInOrder();
